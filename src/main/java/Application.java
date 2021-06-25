db8888a13cd31e3f43161e04d200e28b13adc68c
천광규 <chonkk@castis.com>
2021-06-25 11:20:09 +0900

    Update 스트링을 대체하는 방법 여러가지 시도

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,16 +3,35 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args){
-        String msg = "{\"id\":\"333\",\"passwd\":4545,\"others\":\"test\",\"account_pwd\":\"12345\",\"some\":\"some\"}";
-        System.out.println(msg);
+        String msg = "{\"id\":\"333\",\"passwd\":4545,\"others\":\"test\",\"account_pwd\":\"12345abcd\",\"some\":\"some\"}";
+        System.out.println("main::"+msg);
+        replaceFunction3(msg);
+       replaceFunction1(msg);
+        replaceFunction2(msg);
+    
+        
+    }
+
+    private static void replaceFunction3(String msg) {
+        Pattern pattern = Pattern.compile("\"account_pwd\":\"(\\w*)\"");
+        Matcher matcher = pattern.matcher(msg);
+        if(matcher.find()){
+            String replace = matcher.group(1).replaceAll("\\w","*");
+            String rMsg = "\"account_pwd\":\""+replace+"\"";
+            System.out.println("replaceFunction3::"+matcher.replaceAll(rMsg));
+        }
+    }
+
+    private static void replaceFunction2(String msg) {
+        String maskedPassword = msg.replaceAll("\"account_pwd\":\"(\\w*)\"", "\"account_pwd\":\"****\"");
+        System.out.println("replaceFunction2::"+maskedPassword);
+    }
+
+    private static void replaceFunction1(String msg) {    
         Pattern pattern = Pattern.compile("\"passwd\":(\\d{1,5}),(.*)\"account_pwd\":\"(\\w*)\"");
         Matcher matcher = pattern.matcher(msg);
         if(matcher.find()){
-            System.out.println(matcher.group(1));
-            System.out.println(matcher.groupCount());
-            System.out.println(matcher.group(2));
-            System.out.println(matcher.group(3));
-            System.out.println(matcher.replaceAll("\"passwd\":****,$2\"account_pwd\":\"****\""));
+            System.out.println("replaceFunction1::"+matcher.replaceAll("\"passwd\":****,$2\"account_pwd\":\"****\""));
         }
     }
 }
